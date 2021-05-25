e35e199b0d5e6e4a38a65b8e5ba74dc85931dae7
DooMy <gorun_a@hotmail.com>
2021-05-25 12:30:39 +0300

    Создал классы под реализацию MVVM. Created classes for implementing MVVM.

---
+++ Model/DataWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMWCApp.Model
+{
+    public static class DataWorker
+    {
+        
+        //создать департамент
+
+        //создать сотрудника
+
+        //создать заказ
+
+        //удалить департамент
+
+        //удалить сотрудника
+
+        //удалить заказ
+
+        //редактировать департамент
+
+        //редактировать сотрудника
+
+        //редактировать заказ
+
+    }
+}
